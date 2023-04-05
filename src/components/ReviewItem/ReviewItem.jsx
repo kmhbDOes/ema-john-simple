e28341572d1830af2dc92caaa0699dcd469c4d6d
kmhbDOes <kmhbipul@gmail.com>
2023-04-05 22:45:20 +0600

    Implemented delete function

--- src/components/ReviewItem/ReviewItem.jsx
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./ReviewItem.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faShoppingCart, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 
-const ReviewItem = ({ product }) => {
-  const { img, name, price, quantity } = product;
+const ReviewItem = ({ product, handleRemoveFromCart }) => {
+  const { id, img, name, price, quantity } = product;
   return (
     <div className="review-item">
       <img src={img} alt="" />
@@ -17,7 +17,7 @@ const ReviewItem = ({ product }) => {
           Order Quantity: <span className="orange-text">${quantity}</span>
         </p>
       </div>
-      <button className="btn-delete">
+      <button className="btn-delete" onClick={() => handleRemoveFromCart(id)}>
         <FontAwesomeIcon className="delete-icon" icon={faTrashAlt} />
       </button>
     </div>
